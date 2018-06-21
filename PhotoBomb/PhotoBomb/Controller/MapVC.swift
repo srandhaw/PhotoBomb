9cfcace57908c3b2cc03a08f4e9564b2244c59e6
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-06-21 18:37:20 +0400

    location services done but wron location

--- PhotoBomb/PhotoBomb/Controller/MapVC.swift
+++ PhotoBomb/PhotoBomb/Controller/MapVC.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
+    
+    //Variables
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
 
     //Outlets
     @IBOutlet weak var mapView: MKMapView!
@@ -17,15 +23,50 @@ class MapVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+       self.mapView.showsUserLocation = true
+        configureLocationService()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func centerMapBtnPressed(_ sender: Any) {
+        if(authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse){
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapVC: MKMapViewDelegate{
+    
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius*2, regionRadius*2)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+}
+
+extension MapVC: CLLocationManagerDelegate{
+    
+    func configureLocationService(){
+        if(authorizationStatus == .notDetermined){
+            locationManager.requestAlwaysAuthorization()
+        }
+        else{
+            return
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+    
+    
     
 }
 
